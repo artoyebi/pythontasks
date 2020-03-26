e66a026cd2d46b1f7351933c7fd35abcd5a85ba9
Olaoluwa <61976986+artoyebi@users.noreply.github.com>
2020-03-25 23:35:49 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+import math
+print("Area of a Circle Calculator")
+
+radius=input("Please enter the radius of the circle (cm): ")
+
+area= math.pi * int(radius)**2
+
+print(f"Area of the circle of {radius} is {area} cm")
+
